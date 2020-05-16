0cbd469c4e07ae04e3c3ed583ddd5e1e12dfe67c
Shadman Kaif <kaifshadman@gmail.com>
2020-05-15 21:47:13 -0400

    faster than 81% of solutions

---
+++ LeetCode/RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /*
+    Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+    */
+    int removeDuplicates(vector<int>& nums) {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
