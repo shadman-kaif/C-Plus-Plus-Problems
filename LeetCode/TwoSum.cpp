dc529f18c091cc01056c8a19e5441010b70f00f8
Shadman Kaif <kaifshadman@gmail.com>
2020-05-15 20:13:42 -0400

    Added working two sum, improve efficiency

--- LeetCode/TwoSum.cpp
+++ LeetCode/TwoSum.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
+    
+    /*
+    Given an array of integers, return indices of the two numbers such that they add up to a  specific target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    */
+    
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> index;
+        vector<int> res;
+        bool flag = true;
         for (int i = 0; i < nums.size(); i++) {
-            for (int j = 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    index.push_back(i);
-                    index.push_back(j);
+            if (flag == false) {
+                break;
+            }
+            for (int j = 0; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    flag = false;
                 }
             }
         }
-        return index;
+        return res;
     }
 };
