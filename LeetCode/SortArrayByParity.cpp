45943b7cb724c3609d6aa76ed0f901d6e7d39593
Shadman Kaif <kaifshadman@gmail.com>
2020-05-28 22:46:57 -0400

    added sort array by parity

---
+++ LeetCode/SortArrayByParity.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    /*
+    Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+
+    You may return any answer array that satisfies this condition.
+    */
+    
+    vector<int> sortArrayByParity(vector<int>& A) {
+        vector<int> ans;
+        for (int i = 0; i < A.size(); i++) {
+            if (A[i]%2 == 0) {
+                ans.push_back(A[i]);
+            }
+        }
+        for (int i = 0; i < A.size(); i++) {
+            if (A[i]%2 == 1) {
+                ans.push_back(A[i]);
+            }
+        }
+        return ans;
+    }
+};
